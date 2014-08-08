9404664b958258d3aa1787f0727fe7f86c1bab14
Daniel Margolis <dan@af0.net>
2014-08-09 01:56:36 +0200

    Improve import UI: async import, progress spinner.

--- SesameApp/src/main/java/net/af0/sesame/Common.java
+++ SesameApp/src/main/java/net/af0/sesame/Common.java
@@ -2,10 +2,12 @@ package net.af0.sesame;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.content.FileProvider;
 import android.text.InputType;
 import android.util.Log;
@@ -48,7 +50,7 @@ class Common {
         return true;
     }
 
-    static void onImportKeysResult(final Context ctx, Intent data, final Runnable importCallback) {
+    static void onImportKeysResult(final Activity ctx, Intent data, final Runnable importCallback) {
         final Uri uri = data.getData();
         final InputStream src;
         final OutputStream os;
@@ -73,26 +75,61 @@ class Common {
         final EditText passwordText = new EditText(ctx);
         passwordText.setInputType(InputType.TYPE_CLASS_TEXT |
                 InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        final ProgressDialog progress = new ProgressDialog(ctx);
+        progress.setTitle(R.string.unlock_progress_unlocking);
         AlertDialog.Builder alert = new AlertDialog.Builder(ctx);
         alert.setTitle(ctx.getString(R.string.title_enter_password));
         alert.setMessage(ctx.getString(R.string.import_database_password_message));
         alert.setView(passwordText);
         alert.setPositiveButton(R.string.action_unlock, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
-                try {
-                    char[] password = new char[passwordText.length()];
-                    passwordText.getText().getChars(0, password.length, password, 0);
-                    SQLCipherDatabase.ImportDatabase(ctx, dst.getAbsolutePath(), password);
-                    if (importCallback != null) {
-                        importCallback.run();
+                final char[] password = new char[passwordText.length()];
+                passwordText.getText().getChars(0, password.length, password, 0);
+                AsyncTask<Void, Void, Boolean> importTask = new AsyncTask<Void, Void, Boolean>() {
+                    Exception exception;
+
+                    @Override
+                    protected Boolean doInBackground(Void... params) {
+                        boolean success = false;
+                        ctx.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                progress.show();
+                            }
+                        });
+                        try {
+                            SQLCipherDatabase.ImportDatabase(ctx, dst.getAbsolutePath(), password);
+                            success = true;
+                        } catch (SQLiteException e) {
+                            Log.e("IMPORT", e.toString());
+                            exception = e;
+                        } catch (IOException e) {
+                            Log.e("IMPORT", e.toString());
+                            exception = e;
+                        }
+                        return success;
                     }
-                } catch (SQLiteException e) {
-                    Log.e("IMPORT", e.toString());
-                    DisplayException(ctx, ctx.getString(R.string.import_keys_failure_title), e);
-                } catch (IOException e) {
-                    Log.e("IMPORT", e.toString());
-                    DisplayException(ctx, ctx.getString(R.string.import_keys_failure_title), e);
-                }
+
+                    @Override
+                    protected void onPostExecute(final Boolean success) {
+                        progress.dismiss();
+                        if (success) {
+                            if (importCallback != null) {
+                                importCallback.run();
+                            }
+                            return;
+                        } else {
+                            Log.w("Importing database", exception.toString());
+                            DisplayException(ctx, ctx.getString(R.string.import_keys_failure_title),
+                                    exception);
+                        }
+                    }
+
+                    @Override
+                    protected void onCancelled() {
+                    }
+                };
+                importTask.execute();
             }
         });
         alert.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -104,7 +141,6 @@ class Common {
     }
 
     static void StartImportKeys(Activity ctx) {
-        // TODO: show a progress spinner here.
         Intent importIntent = new Intent();
         importIntent.setAction(Intent.ACTION_GET_CONTENT);
         importIntent.setType("*/*");
